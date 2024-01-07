68696a8e49a6e4c3fdf6afc0e6e71b8a8ca9fad9
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-01-07 02:05:44 +0200

    created configure for airtable

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,14 @@
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_KEY);
+
+const table = base('coffee-stores');
+
+console.log({table})
+
+const createCoffeeStore = (req, res) => {
+    res.json({message: 'Hi there!'})
+}
+
+export default createCoffeeStore;
+
+
